94d30d9d9ada60927304fc40dd943b91af55b17b
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-22 10:19:35 +0100

    gg

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import ConsigliatoPerTe from "./components/ConsigliatoPerTe";
 
 function App() {
     return (
@@ -10,6 +12,7 @@ function App() {
                 <Row>
                     <Col xs={12} md={8} lg={9}>
                         {/* informazioni Profilo qui dentro  */}
+                        <ConsigliatoPerTe />
                     </Col>
                 </Row>
             </Container>
---
+++ react-app/src/components/ConsigliatoPerTe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const ConsigliatoPerTe = () => {
+    return <></>;
+};
+
+export default ConsigliatoPerTe;

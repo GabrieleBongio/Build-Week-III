e45145a8507e49a1c180aeb5e6a0189b8b3fa7e9
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-22 11:51:06 +0100

    informazioni profilo

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,19 +4,20 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ConsigliatoPerTe from "./components/ConsigliatoPerTe";
+import InformazioniProfilo from "./components/InformazioniProfilo";
 
 function App() {
     return (
-        <div className="App">
-            <Container>
-                <Row>
-                    <Col xs={12} md={8} lg={9}>
-                        {/* informazioni Profilo qui dentro  */}
-                        <ConsigliatoPerTe />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+        /*  <div className="App"> */
+        <Container>
+            <Row>
+                <Col xs={12} md={7} lg={9}>
+                    <InformazioniProfilo />
+                    {/* <ConsigliatoPerTe /> */}
+                </Col>
+            </Row>
+        </Container>
+        /*  </div> */
     );
 }
 
--- react-app/src/components/InformazioniProfilo.jsx
+++ react-app/src/components/InformazioniProfilo.jsx
@@ -1,9 +1,101 @@
 import React from "react";
+import Row from "react-bootstrap/esm/Row";
+import { CameraFill, Pen, PatchCheckFill, PenFill, Camera2 } from "react-bootstrap-icons";
+import Col from "react-bootstrap/esm/Col";
+import { NavLink } from "react-router-dom";
 
 const InformazioniProfilo = () => {
     return (
         <>
-            <div></div>
+            <div>
+                <Row>
+                    {/* prima sezione  */}
+                    <Col xs={12}>
+                        <div className="position-relative">
+                            <div className="bg-image d-flex justify-content-end">
+                                <div className="p-3">
+                                    <CameraFill className="fs-3 text-light" />
+                                </div>
+                            </div>
+                            {/* div in absolute  */}
+                            <div className="rounded-circle d-inline-block position-absolute custom-top custom-left bg-secondary-custom">
+                                {" "}
+                                <Camera2 className="fs-1 m-5" />
+                            </div>
+                        </div>
+                    </Col>
+                    {/* seconda sezione */}
+                    <Col xs={12}>
+                        <Row>
+                            <Col>
+                                <div className=" d-flex justify-content-end">
+                                    <div className="p-2">
+                                        <Pen className="m-2 fs-6" />
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <div className="d-flex px-3">
+                                    <div className="d-block d-lg-flex align-items-center gap-2">
+                                        <h4>Nome Cognome</h4>
+
+                                        <div className="border border-primary rounded-pill px-2">
+                                            <PatchCheckFill className="text-primary me-1" />
+                                            Inizia La verifica{" "}
+                                        </div>
+                                    </div>
+                                    <div className="d-flex align-items-center m-auto">
+                                        <img
+                                            width={"40px"}
+                                            src="https://media.licdn.com/dms/image/C4E0BAQHYgix-Ynux1A/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1714003200&amp;v=beta&amp;t=02cZOkAFfrcsqE3vMctwQcElNrMnInX4NwQFmaTF1M8"
+                                            alt="epicode logo "
+                                        />
+                                        <h4>EPICODE</h4>
+                                    </div>
+                                </div>
+                                <div className="px-3">
+                                    <p className="mb-1">Studente Presso Epicode, Epicode Education </p>
+                                </div>
+                                <div className="px-3 d-flex gap-2">
+                                    <p className="mb-1"> Roma, Lazio, 00133 </p>
+                                    <p>Informazioni di contatto</p>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <div className="d-block d-lg-flex gap-1">
+                                    <div className="px-3 border rounded-pill bg-primary text-light">
+                                        <p className="m-0 p-1">Disponibile Per</p>
+                                    </div>
+                                    <div className="px-3 border rounded-pill">
+                                        <p className="m-0 p-1">Aggiungi Sezione del Profilo</p>
+                                    </div>
+                                    <div className="px-3 border rounded-pill">
+                                        <p className="m-0 p-1">Aggiungi Pulsante personalizzato</p>
+                                    </div>
+                                    <div className="px-3 border rounded-pill">
+                                        <p className="m-0 p-1">Altro</p>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={8} sm={8} md={8} lg={6} xl={6} xxl={6}>
+                                {" "}
+                                <div className="sfondo-chiaro rounded rouded-5 m-3 p-2 position-relative">
+                                    <p className="m-1">disponibile a lavorare</p>
+                                    <p className="m-1">Ruoli di programmatore</p>
+                                    <p className="m-1">Mostra dettagli</p>
+                                    <PenFill className="m-3 position-absolute top-0 end-0" />
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </div>
         </>
     );
 };

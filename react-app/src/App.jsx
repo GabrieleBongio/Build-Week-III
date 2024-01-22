a24a603fab29e1a7f6533a05145016b1adad5044
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-22 10:12:24 +0100

    gg

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Container>
+                <Row>
+                    <Col xs={12} md={8} lg={9}>
+                        {/* informazioni Profilo qui dentro  */}
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;

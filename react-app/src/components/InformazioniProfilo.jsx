6cadbbdaea23bd47fa21ba9837a8f60693050b13
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-22 10:21:33 +0100

    gg

--- react-app/src/components/InformazioniProfilo.jsx
+++ react-app/src/components/InformazioniProfilo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const InformazioniProfilo = () => {
-    return <></>;
+    return (
+        <>
+            <div></div>
+        </>
+    );
 };
 
 export default InformazioniProfilo;

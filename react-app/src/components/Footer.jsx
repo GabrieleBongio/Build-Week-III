05da154cb457285d7a868a324a7f9334bbb09445
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-23 09:29:12 +0100

    gg

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Container, Nav, NavDropdown, Row } from "react-bootstrap";
 import DropdownToggle from "react-bootstrap/DropdownToggle";
+import { QuestionCircleFill, ShieldFillCheck, GearFill } from "react-bootstrap-icons";
 
 const Footer = () => {
     return (
@@ -69,29 +70,53 @@ const Footer = () => {
                     </Col>
                     <Col>
                         <div className="mb-3">
-                            <p className="m-1">Lorem, ipsum.</p>
-                            <p className="m-1">Lorem, ipsum.</p>
+                            <div className="d-flex gap-2">
+                                <div>
+                                    <QuestionCircleFill className="fs-2" />
+                                </div>
+                                <div className="d-flex flex-column">
+                                    <p className="m-0">Domande.</p>
+                                    <p className="m-0">Visita il nostro centro assistenza</p>
+                                </div>
+                            </div>
                         </div>
                         <div className="mb-3">
-                            <p className="m-1">Lorem, ipsum.</p>
-                            <p className="m-1">Lorem, ipsum.</p>
+                            <div className="d-flex gap-2">
+                                <div>
+                                    <GearFill className="fs-2" />
+                                </div>
+                                <div className="d-flex flex-column">
+                                    <p className="m-0">Domande.</p>
+                                    <p className="m-0">Visita il nostro centro assistenza</p>
+                                </div>
+                            </div>
                         </div>
                         <div className="mb-3">
-                            <p className="m-1">Lorem, ipsum.</p>
-                            <p className="m-1">Lorem, ipsum.</p>
+                            <div className="d-flex gap-2">
+                                <div>
+                                    <ShieldFillCheck className="fs-2" />
+                                </div>
+                                <div className="d-flex flex-column">
+                                    <p className="m-0">Domande.</p>
+                                    <p className="m-0">Visita il nostro centro assistenza</p>
+                                </div>
+                            </div>
                         </div>
                     </Col>
                     <Col>
-                        {" "}
-                        {/* <Dropdown className="d-inline mx-2">
-                            <Dropdown.Toggle id="dropdown-autoclose-true">Default Dropdown</Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-                                <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-                                <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown> */}
+                        <Row>
+                            <Col>
+                                <Nav>
+                                    <NavDropdown id="nav-dropdown-dark-example" title="Dropdown" menuVariant="dark">
+                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
